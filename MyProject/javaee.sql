a364cdd332b124244c2c6eb79b2f322c5b80b748
GJF4156 <gjf13684433731@163.com>
2019-12-17 21:58:48 +0800

    javaEE期末项目带数据库完整版

--- MyProject/javaee.sql
+++ MyProject/javaee.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80015
 File Encoding         : 65001
 
-Date: 2019-12-14 17:56:30
+Date: 2019-12-17 21:57:46
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,16 +29,18 @@ CREATE TABLE `address` (
   KEY `content` (`content`),
   KEY `fm_address_user_createuid` (`createuid`),
   CONSTRAINT `fm_address_user_createuid` FOREIGN KEY (`createuid`) REFERENCES `user` (`uid`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of address
 -- ----------------------------
 INSERT INTO `address` VALUES ('2', '郭建风', '17721901419', '德馨苑C3-225', '1');
-INSERT INTO `address` VALUES ('14', '小强', '13644446666', '实验楼\r\n', '2');
-INSERT INTO `address` VALUES ('17', '郭建风', '13684433731', '图书馆', '2');
+INSERT INTO `address` VALUES ('17', '郭建风', '13684433731', '三教图书馆', '2');
 INSERT INTO `address` VALUES ('20', 'test', '13777777777', '测试地址', '15');
-INSERT INTO `address` VALUES ('22', '1', '1', '1111', '17');
+INSERT INTO `address` VALUES ('22', '测试地址', '01234567891', '测试地址', '17');
+INSERT INTO `address` VALUES ('24', '小峰同学', '13197819248', 'C9-321', '2');
+INSERT INTO `address` VALUES ('25', '郭建风', '13881188257', 'C3', '20');
+INSERT INTO `address` VALUES ('26', '嘿嘿', '17721901419', 'C10-321', '21');
 
 -- ----------------------------
 -- Table structure for forum
@@ -46,17 +48,25 @@ INSERT INTO `address` VALUES ('22', '1', '1', '1111', '17');
 DROP TABLE IF EXISTS `forum`;
 CREATE TABLE `forum` (
   `fid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `publishUid` int(11) NOT NULL,
+  `publishUser` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `content` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `date` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`fid`),
-  KEY `fl_ForumAndUser_username` (`username`),
-  CONSTRAINT `fl_ForumAndUser_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  KEY `fl_ForumAndUser_username` (`publishUid`),
+  CONSTRAINT `fl_ForumAndUser_username` FOREIGN KEY (`publishUid`) REFERENCES `user` (`uid`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of forum
 -- ----------------------------
+INSERT INTO `forum` VALUES ('7', '2', '小峰同学', '        哈哈哈哈哈哈哈哈哈', '2019-12-15 00:06');
+INSERT INTO `forum` VALUES ('8', '2', '小峰同学', '        测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下测试一下', '2019-12-15 00:07');
+INSERT INTO `forum` VALUES ('9', '15', 'test', '        喜喜喜喜喜喜', '2019-12-15 00:36');
+INSERT INTO `forum` VALUES ('10', '2', '小峰同学', '        bhsdfgghfgn', '2019-12-15 20:34');
+INSERT INTO `forum` VALUES ('11', '20', '苦其心志', '        你心中', '2019-12-15 21:21');
+INSERT INTO `forum` VALUES ('12', '2', '小峰同学', '        测试发帖        测试发帖        测试发帖', '2019-12-16 19:00');
+INSERT INTO `forum` VALUES ('13', '2', '小峰同学', '        我来试试发帖', '2019-12-16 19:03');
 
 -- ----------------------------
 -- Table structure for messige
@@ -68,7 +78,7 @@ CREATE TABLE `messige` (
   `phone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `suggest` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`mid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of messige
@@ -77,6 +87,9 @@ INSERT INTO `messige` VALUES ('4', '张三', '13684433731', 'agfedhsrgjhtjfg');
 INSERT INTO `messige` VALUES ('5', '张三', '13684433731', 'agfedhsrgjhtjfg');
 INSERT INTO `messige` VALUES ('6', '张三', '13684433731', 'agfedhsrgjhtjfg');
 INSERT INTO `messige` VALUES ('7', '郭建风', '13684433731', '测试留言');
+INSERT INTO `messige` VALUES ('8', '小峰同学', '13778358345', '测试发一条建议');
+INSERT INTO `messige` VALUES ('9', '和你们', '649494649464', '你以为');
+INSERT INTO `messige` VALUES ('10', '小唐', '17721901419', '测试留言');
 
 -- ----------------------------
 -- Table structure for news
@@ -116,15 +129,21 @@ CREATE TABLE `orders` (
   PRIMARY KEY (`oid`),
   KEY `fk_user_order_uid` (`username`),
   KEY `fk_OrderAndUser` (`receipt`),
-  KEY `fk_order_address_address` (`address`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  KEY `fk_order_address_address` (`address`),
+  KEY `fk_orders_user_uid` (`uid`),
+  CONSTRAINT `fk_orders_user_uid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of orders
 -- ----------------------------
-INSERT INTO `orders` VALUES ('40', '15', 'test', '---请选择---', 'assdddddddddddddddddddd', 'test 13777777777 测试地址', '不限', '2.00', '未处理', '2019-12-13 19:20', '2019-12-13  19:20', '-1');
-INSERT INTO `orders` VALUES ('41', '2', '郭建风', '求陪跑', 'sdcfafsdgfsdfghdf', '小强 13644446666 实验楼', '不限', '3.00', '未处理', '2019-12-13 19:39', '2019-12-13  19:39', '-1');
-INSERT INTO `orders` VALUES ('42', '17', '1', '取快递', '11', '1 1 1111', '限美女', '2.00', '已完成', '2019-12-13 19:48', '2019-12-02  19:47', '2');
+INSERT INTO `orders` VALUES ('44', '2', '小峰同学', '求帮忙', '帮忙去后街取东西', '郭建风 13684433731 图书馆', '不限', '5.00', '进行中', '2019-12-15 20:56', '2019-12-15  21:55', '17');
+INSERT INTO `orders` VALUES ('46', '2', '小峰同学', '取快递', '快递超市取快递，有点重', '郭建风 13684433731 图书馆', '限男生', '8.00', '已完成', '2019-12-15 20:58', '2019-12-15  22:58', '17');
+INSERT INTO `orders` VALUES ('47', '17', '1', '求带饭', '测试发布任务', '测试地址 01234567891 测试地址', '限美女', '2.00', '进行中', '2019-12-15 21:16', '2019-12-15  21:16', '20');
+INSERT INTO `orders` VALUES ('48', '17', '1', '取快递', '好脸色虎扑', '测试地址 01234567891 测试地址', '限美女', '2.00', '已完成', '2019-12-15 21:20', '2019-12-15  21:20', '20');
+INSERT INTO `orders` VALUES ('49', '21', '嘿嘿', '求帮忙', '帮忙指导作业', '嘿嘿 17721901419 C10-321', '限美女', '3.00', '进行中', '2019-12-16 12:08', '2019-12-16  19:08', '2');
+INSERT INTO `orders` VALUES ('50', '2', '小峰同学', '求帮忙', '帮忙', '郭建风 13684433731 C3-225', '不限', '2.00', '未处理', '2019-12-16 19:06', '2019-12-16  19:06', '-1');
+INSERT INTO `orders` VALUES ('51', '2', '小峰同学', '求帮忙', '送资料', '郭建风 13684433731 三教图书馆', '不限', '2.00', '未处理', '2019-12-16 21:47', '2019-12-16  21:47', '-1');
 
 -- ----------------------------
 -- Table structure for reply
@@ -132,17 +151,47 @@ INSERT INTO `orders` VALUES ('42', '17', '1', '取快递', '11', '1 1 1111', '
 DROP TABLE IF EXISTS `reply`;
 CREATE TABLE `reply` (
   `rid` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
+  `fid` int(255) NOT NULL,
+  `uid` int(255) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `content` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `reply_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `replyTime` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`rid`),
+  KEY `fk_reply_forum_fid` (`fid`),
   KEY `fk_ReplyAndUser_uid` (`uid`),
-  CONSTRAINT `fk_ReplyAndUser_uid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  CONSTRAINT `fk_ReplyAndUser_uid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_reply_forum_fid` FOREIGN KEY (`fid`) REFERENCES `forum` (`fid`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of reply
 -- ----------------------------
+INSERT INTO `reply` VALUES ('34', '10', '2', '小峰同学', '第一条回复', '2019-12-16 19:03');
+INSERT INTO `reply` VALUES ('35', '12', '2', '小峰同学', '测试回帖', '2019-12-16 19:03');
+INSERT INTO `reply` VALUES ('36', '12', '2', '小峰同学', '哈哈', '2019-12-16 21:46');
+INSERT INTO `reply` VALUES ('37', '11', '2', '小峰同学', '啦啦啦', '2019-12-16 21:47');
+INSERT INTO `reply` VALUES ('38', '8', '2', '小峰同学', '啦啦啦啦啦', '2019-12-16 21:47');
+
+-- ----------------------------
+-- Table structure for stucard
+-- ----------------------------
+DROP TABLE IF EXISTS `stucard`;
+CREATE TABLE `stucard` (
+  `cid` int(255) NOT NULL AUTO_INCREMENT,
+  `uid` int(255) NOT NULL,
+  `realName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `stuNumber` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
+  `cardImage` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`cid`),
+  KEY `fk_stucard_user_uid` (`uid`),
+  CONSTRAINT `fk_stucard_user_uid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of stucard
+-- ----------------------------
+INSERT INTO `stucard` VALUES ('1', '2', '郭建风', '41701034', 'ef699ed9-3717-4323-b248-a5096287c7a6.png');
+INSERT INTO `stucard` VALUES ('2', '2', '郭建风', '41701034', '56783d75-830d-4c53-b6ac-28d01bbba934.png');
 
 -- ----------------------------
 -- Table structure for user
@@ -156,15 +205,17 @@ CREATE TABLE `user` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`uid`),
   KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES ('1', '小唐', '13778787979', '111', 'jjj@qq.com');
-INSERT INTO `user` VALUES ('2', '郭建风', '13684433731', 'gjf.33731', 'gjf13684433731@163.com');
+INSERT INTO `user` VALUES ('2', '小峰同学', '13684433731', 'gjf.33731', 'gjf13684433731@163.com');
 INSERT INTO `user` VALUES ('14', '王五', '17721191109', '555555', 'ww@qq.com');
-INSERT INTO `user` VALUES ('15', 'test', '13666666666', '123456', 'test@qq.com');
+INSERT INTO `user` VALUES ('15', 'test', '13666666666', 'aaa', 'test@qq.com');
 INSERT INTO `user` VALUES ('16', '12122', '122221', '12121', '982121915@qq.com');
 INSERT INTO `user` VALUES ('17', '1', '123', '111', '982121915@qq.com');
 INSERT INTO `user` VALUES ('18', '张三', '17721901419', '1111', 'zs@qq.com');
+INSERT INTO `user` VALUES ('20', '苦其心志', '13881188257', 'qwer1234', '8494@qq.com');
+INSERT INTO `user` VALUES ('21', '嘿嘿', '17721901419', 'gjf.33731', 'aa@163.com');
